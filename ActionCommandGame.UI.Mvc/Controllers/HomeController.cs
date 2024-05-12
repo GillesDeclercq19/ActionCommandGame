202726ba955336522c6bf38adce05f697ba4e701
Stockey18 <gilles.declercq2@student.vives.be>
2024-05-12 20:27:13 +0200

    Design1

--- ActionCommandGame.UI.Mvc/Controllers/HomeController.cs
+++ ActionCommandGame.UI.Mvc/Controllers/HomeController.cs
@@ -1,26 +1,13 @@
 using ActionCommandGame.UI.Mvc.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-
 namespace ActionCommandGame.UI.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-
-        public HomeController()
-        {
-
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
     }
 }
