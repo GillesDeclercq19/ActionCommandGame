5cbf0b59fce2049fcb8c48b594ce32783ecc45bc
Stockey18 <gilles.declercq2@student.vives.be>
2024-05-25 11:22:20 +0200

    Inventory

--- ActionCommandGame.Services.Model/Results/PlayerResult.cs
+++ ActionCommandGame.Services.Model/Results/PlayerResult.cs
@@ -10,16 +10,12 @@ namespace ActionCommandGame.Services.Model.Results
         public int Zeni { get; set; }
         public int Experience { get; set; }
         public DateTime LastActionExecutedDateTime { get; set; }
-
         public int? CurrentKiPlayerItem { get; set; }
-        public string? CurrentKiPlayerItemName { get; set; }
+        public string? KiItemName { get; set; }
         public int? CurrentAttackPlayerItem { get; set; }
-        public string? CurrentAttackPlayerItemName { get; set; }
+        public string? AttItemName { get; set; }
         public int? CurrentDefensePlayerItem { get; set; }
-        public string? CurrentDefensePlayerItemName { get; set; }
-
-
-     
+        public string? DefItemName { get; set; }
         public IList<PlayerItemResult> Inventory { get; set; }
         public string UserId { get; set; }
     }
